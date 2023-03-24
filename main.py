12f278933bcc146dd5b98cc62215e74fde564e79
Alon Treitel <122409210+alontre@users.noreply.github.com>
2023-03-24 14:50:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-    @staticmethod
-    def start_service():
-        from jnius import autoclass
-        service = autoclass("org.mindset.codered.ServiceCodered")
-        mActivity = autoclass("org.kivy.android.PythonActivity").mActivity
-        service.start(mActivity, "")
-        return service
+from multiprocessing.dummy import Process
+        
+@staticmethod
+def start_service():
+    from jnius import autoclass
+    service = autoclass("org.mindset.codered.ServiceCodered")
+    mActivity = autoclass("org.kivy.android.PythonActivity").mActivity
+    service.start(mActivity, "")
+    return service
+def main():
+    start_service()
