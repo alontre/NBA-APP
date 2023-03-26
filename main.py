c8599e775653566d624be45cf78ae42fc27dc9e6
Alon Treitel <122409210+alontre@users.noreply.github.com>
2023-03-26 15:32:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ from nba_api.live.nba.endpoints import scoreboard
 from datetime import datetime, timezone, timedelta
 from dateutil import parser
 from kivy.app import App
-from kivy.core.audio import SoundLoader
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-from kivy.clock import Clock
-from kivy.config import Config
+from kivy.core.window import Window
 
-# Set the graphics configuration for the app
-Config.set('graphics', 'width', 'auto')
-Config.set('graphics', 'height', 'auto')
+Window.size = (360, 600)
 
 # Define a function to return only some values of a dictionary
 def only_print_some(array):
@@ -95,19 +91,10 @@ def get_games(half):
 
     # Loop through each game and add it to the games_today dictionary
     for game in games:
-        if half == "second": # If looking for second half of a day games
-            if i > int(len(games) * 0.5): # Only add games from the second half of the day
-                # Get the game time and add the game to the games_today dictionary
-                game_time = str(parser.parse(game["gameTimeUTC"]).replace(tzinfo=timezone.utc).astimezone())[11:16]
-                games_today[i] = [i, (game['awayTeam']['teamName'] + " vs " + game['homeTeam']['teamName']), game_time,
-                                  game['gameId']]
-        else: # If looking for first half of a day games
-            # Get the game time and add the game to the games_today dictionary
-            game_time = str(parser.parse(game["gameTimeUTC"]).replace(tzinfo=timezone.utc).astimezone())[11:16]
-            games_today[i] = [i, (game['awayTeam']['teamName'] + " vs " + game['homeTeam']['teamName']), game_time,
-                              game['gameId']]
-            if i > (len(games)) * 0.5 - 1 and half == "first": # Only add games from the first half of the day
-                break # If we've added all the first half games, break out of the loop
+        # Get the game time and add the game to the games_today dictionary
+        game_time = str(parser.parse(game["gameTimeUTC"]).replace(tzinfo=timezone.utc).astimezone())[11:16]
+        games_today[i] = [i, (game['awayTeam']['teamName'] + " vs " + game['homeTeam']['teamName']), game_time,
+                          game['gameId']]
 
         i += 1 # Increment the counter
 
@@ -116,6 +103,7 @@ def get_games(half):
         return print_table(games_today)
     else: # Otherwise, return the games_today dictionary
         return games_today
+
 
 
 
@@ -133,9 +121,10 @@ def new_dict(my_dict, choice):
 
 
 # Create a Kivy App
-class MyApp(App):
+class NBA(App):
     # Build the App's UI
     def build(self):
+        self.icon = "nbaicon.png"
         # Set the time to 0
         self.time = 0
         # Create a layout to hold the widgets
@@ -143,24 +132,26 @@ class MyApp(App):
 
         # Create a horizontal layout for the labels
         self.label_layout = BoxLayout(orientation='horizontal')
-
         # Create three labels with different text
-        self.label1 = Label(text=get_games("first"), font_size='13sp')
-        self.label2 = Label(text=get_games("second"), font_size='13sp')
-        self.label3 = Label(text="\n\n\n\nPlease enter the game/s I'd/s\n with a comma separator.",
-                            font_size='15sp')
+        self.label1 = Label(text=get_games("first"), font_size='16', color='#00FFCE', pos_hint={"center_x": 0.5, "center_y": 0.25})
 
-        # Add the labels to the horizontal layout
         self.label_layout.add_widget(self.label1)
-        self.label_layout.add_widget(self.label3)
-        self.label_layout.add_widget(self.label2)
+
+        self.label_layout_two = BoxLayout(orientation='horizontal')
+
+        self.label3 = Label(text="Please enter the game/s I'd/s\n with a comma separator.",
+                            font_size='16', color='#00FFCE', pos_hint={"center_x": 0.5, "center_y": 0.45})
+
+        self.label_layout_two.add_widget(self.label3)
+
 
         # Add the horizontal layout to the main layout
         self.layout.add_widget(self.label_layout)
+        self.layout.add_widget(self.label_layout_two)
 
         # Create text input widgets for the games and maximum time
-        self.games_input = TextInput(text="", size_hint=(3, 0.2))
-        self.max_time_input = TextInput(text="", size_hint=(3, 0.2))
+        self.games_input = TextInput(text="", multiline=False, size_hint=(0.6, 0.2), padding_y=(6, 6), pos_hint={"center_x": 0.5, "center_y": 0.5})
+        self.max_time_input = TextInput(text="", multiline=False, size_hint=(0.6, 0.2),  padding_y=(6, 6), pos_hint={"center_x": 0.5, "center_y": 0.5})
 
         # Add the text input widgets to the main layout
         self.layout.add_widget(self.games_input)
@@ -170,12 +161,14 @@ class MyApp(App):
         self.max_time_input.opacity = 0
 
         # Create a button to submit the form
-        self.submit_button = Button(text="Submit", on_release=self.first_submit, size_hint=(0.5, 0.3),
-                                    pos_hint={'x': 0.25, 'y': 0.3})
+        self.submit_button = Button(text="OK", bold=True, background_color='#00FFCE', on_release=self.first_submit
+                                    , size_hint=(0.6, 0.5),
+                                    pos_hint={"center_x": .5})
+
         self.layout.add_widget(self.submit_button)
 
         # Create a label to display the chosen games
-        self.chosen_games_label = Label(text="")
+        self.chosen_games_label = Label(text="chosen games:\n----------------------\n", color='#00FFCE', font_size='20')
         self.layout.add_widget(self.chosen_games_label)
 
         # Return the main layout
@@ -193,17 +186,19 @@ class MyApp(App):
                     raise("")
 
                 # Display only the chosen games
-                self.chosen_games_label.text = str(only_print_some(new_dict(get_games("ere"), self.chosen_games)))
+                self.chosen_games_label.text = "chosen games:\n----------------------\n" + str(only_print_some(new_dict(get_games("ere"), self.chosen_games)))
 
             except:
                 # Display an error message if the input is invalid
-                self.label3.text = "\n\n\n\nPlease enter the game/s I'd/s\n with a comma separator.\nError with input. Go again"
+                self.label3.text = "Please enter the game/s I'd/s\n with a comma separator.\n\n  Error with input try again."
 
             else:
                 # If the input is valid, update the labels and show the next input widget
                 self.label1.text = ""
-                self.label2.text = ""
-                self.label3.text = "Please enter the point difference (in the first area).\n And the time left on the clock (in the second area)."
+                self.label3.font_size = 20
+                self.label3.pos_hint = {"center_x": 0.5, "center_y": 0.85}
+                self.label3.text = "Please enter the point difference\n           (in the first area).\n\n And the time left on the clock" \
+                                   "\n           (in the second area)."
                 self.games_input.text = ""
                 self.max_time_input.opacity = 1
                 self.time += 1
@@ -224,7 +219,10 @@ class MyApp(App):
             self.max_time = int(self.max_time_input.text) # Store the max time left in the clock as an integer
         except:
             # If there is an exception, display an error message
-            self.label3.text = "Please enter the point difference (in first area).\n And the time left in clock (in second area).\nError with input, go again"
+            self.label3.font_size = 20
+            self.label3.pos_hint = {"center_x": 0.5, "center_y": 0.85}
+            self.label3.text = "Please enter the point difference\n           (in the first area).\n\n And the time left on the clock" \
+                               "\n           (in the second area).\n\n      Error with input, go again."
         else:
             # If there is no exception, remove the input widgets and submit button
             self.layout.remove_widget(self.submit_button)
@@ -237,7 +235,7 @@ class MyApp(App):
     def wait_to_the_games(self, games_today):
         global is_time_right
         # Update the label with the chosen game time and points
-        self.chosen_games_label.text = f"\nchosen time:{self.max_time_input.text}       chosen poins:{self.games_input.text}"
+        self.chosen_games_label.text = f"\nchosen time:{self.max_time_input.text}       chosen points:{self.games_input.text}"
 
         # Store the games for the day
         self.games_today = games_today
@@ -270,8 +268,5 @@ class MyApp(App):
         service.start(mActivity, "")
         return service
 
-
-
-
 if __name__ == "__main__":
-    MyApp().run()
+    NBA().run()
