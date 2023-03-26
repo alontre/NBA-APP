0309f4cc4cc3c70079be335fd12b57db00b70b66
Alon Treitel <122409210+alontre@users.noreply.github.com>
2023-03-26 19:35:36 +0300

    Update service.py

--- service.py
+++ service.py
@@ -1,11 +1,15 @@
 from jnius import autoclass
-import plyer
-PythonService = autoclass('org.kivy.android.PythonService')
-PythonService.mService.setAutoRestartService(True)
 from __main__ import *
 from nba_api.live.nba.endpoints import scoreboard
 from kivy.core.audio import SoundLoader
-from kivy.clock import Clock
+from time import sleep
+import plyer
+
+PythonService = autoclass('org.kivy.android.PythonService')
+PythonService.mService.setAutoRestartService(True)
+
+plyer.notification.notify(title="background", message="background service now running...")
+
 def game():
     # Create a new scoreboard object
     board = scoreboard.ScoreBoard()
@@ -18,13 +22,14 @@ def game():
 
     # Print the date of the scoreboard
     # Get the games to display from the games_today attribute of the object
-    games_wanted = App.games_today
+    games_wanted = Nba.games_today
 
     # Iterate through all the games in the scoreboard
     for i in range(len(games["scoreboard"]["games"])):
 
         # Check if the game has started
-        if games["scoreboard"]["games"][i]["gameStatusText"][1:2].isnumeric() and games["scoreboard"]["games"][i]["gameStatusText"][6:7].isnumeric():
+        if games["scoreboard"]["games"][i]["gameStatusText"][1:2].isnumeric() and \
+                games["scoreboard"]["games"][i]["gameStatusText"][6:7].isnumeric():
 
             # Iterate through the games we want to display
             for ii in range(len(list(games_wanted.values()))):
@@ -33,7 +38,8 @@ def game():
                 if games["scoreboard"]["games"][i]["gameId"] == list(games_wanted.values())[ii][3]:
 
                     # Calculate the game time in minutes
-                    game_time = int(games["scoreboard"]["games"][i]["gameStatusText"][1:2]) * 12 - 12 + int(games["scoreboard"]["games"][i]["gameStatusText"][3:5])
+                    game_time = int(games["scoreboard"]["games"][i]["gameStatusText"][1:2]) * 12 - 12 + \
+                                int(games["scoreboard"]["games"][i]["gameStatusText"][3:5])
 
                     # Get the scores of the two teams
                     score1 = int(games["scoreboard"]["games"][i]["homeTeam"]["score"])
@@ -41,14 +47,16 @@ def game():
 
                     # Add the game status and team names and scores to the text string
                     text += str(games["scoreboard"]["games"][i]["gameStatusText"]) + "\n"
-                    text += str(str(games["scoreboard"]["games"][i]["homeTeam"]["teamName"]) + "-" + str(score1) + ":" + str(score2) + "-" + str(games["scoreboard"]["games"][i]["awayTeam"]["teamName"]))
+                    text += str(str(games["scoreboard"]["games"][i]["homeTeam"]["teamName"]) + "-" + str(score1) + ":" +
+                                str(score2) + "-" + str(games["scoreboard"]["games"][i]["awayTeam"]["teamName"]))
 
                     # Check if the score difference is within the specified range
-                    is_score_good2 = score1 - score2 <= App.max_score and score1 - score2 >= 0
-                    is_score_good = score2 - score1 <= App.max_score and score2 - score1 >= 0
+                    is_score_good2 = score1 - score2 <= Nba.max_score and score1 - score2 >= 0
+                    is_score_good = score2 - score1 <= Nba.max_score and score2 - score1 >= 0
 
                     # Check if the game time and score is within the specified range
-                    if list(games_wanted.values())[ii][0] != 100 and (is_score_good or is_score_good2) and (MyApp.max_time + game_time) > 48:
+                    if list(games_wanted.values())[ii][0] != 100 and (is_score_good or is_score_good2) and (
+                            Nba.max_time + game_time) > 48:
 
                         # Mark the game as displayed
                         list(games_wanted.values())[ii][0] = 100
@@ -59,7 +67,8 @@ def game():
                             sound.play()
 
 
-    # Schedule the game method to be called again after 10 seconds
-    Clock.schedule_once(game, 10)
 
-Clock.schedule_once(game, int(60 * is_time_right))
+while True:
+    print("service is running...")
+    game()
+    sleep(10)
